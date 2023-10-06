dc7bfc28d649d813814bd17ba977cd387c4d112b
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-10-06 12:10:08 -0700

    completed the navbar layout, and added a form that is rendered once the user clicks the search Icon. The form currently only captures the user input, clears the field, and logs the value.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,11 @@
-import { useState } from "react";
 import "./App.css";
-import twistedTrout from "./assets/images/twisted-trout.svg";
-import search from "./assets/images/magnifying-glass.svg";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const [displaySearch, setDisplaySearch] = useState(false);
-
   return (
-    <>
-      <nav className='content-flex'>
-        <div className='search-wrapper'>
-          <img
-            src={search}
-            onClick={() =>
-              displaySearch ? setDisplaySearch(false) : setDisplaySearch(true)
-            }
-          />
-        </div>
-        <div className='image-wrapper'>
-          <span>Twisted</span>
-          <img src={twistedTrout} />
-          <span>Trout</span>
-        </div>
-        <div className='user-icons-wrapper'>
-          <img src='wishlist' />
-          <img src='shopping-bag' />
-          <img src='account' />
-        </div>
-      </nav>
-    </>
+    <div className='continer-responsive'>
+      <Navbar />
+    </div>
   );
 }
 
