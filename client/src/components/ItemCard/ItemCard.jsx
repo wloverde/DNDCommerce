a45f7e13afa0c4ba58d7b4acd74634f3382d9563
Loverde <w.andrew.loverde@gmail.com>
2023-10-09 15:29:43 -0500

    Added Item card with minor styling as main display

---
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ItemCard = ({ itemName, itemImage, itemTags, itemPrice, itemStock }) => {
+    return (
+        <div className='item-card'>
+            <h2>{itemName}</h2>
+            <img src={itemImage} alt={itemName} />
+            <p>Tags: {itemTags.join(', ')}</p>
+            <p>Price: ${itemPrice}</p>
+            <p>Stock: {itemStock}</p>
+        </div>
+    );
+};
+
+export default ItemCard;
