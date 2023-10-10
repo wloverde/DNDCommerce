8b77fae2e640c2bf3721a7e6f8daaab33826f119
Loverde <w.andrew.loverde@gmail.com>
2023-10-10 15:34:03 -0500

    fixed router-dom to properly switch

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 // import LoginPage from './LoginPage'; // Import your login page component
 import ItemPage from './pages/ItemPage'; // Import your item description page component
@@ -7,13 +7,13 @@ import ItemPage from './pages/ItemPage'; // Import your item description page co
 const AppRouter = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
         {/* Define your routes */}
         <Route path="/" exact component={App} />
         {/* <Route path="/login" component={LoginPage} /> */}
         <Route path="/item/:itemId" component={ItemPage} />
         {/* Add more routes for other pages as needed */}
-      </Switch>
+      </Routes>
     </Router>
   );
 };
