eb3ef599b72b8cdd2c79e2d778d959db8dda99f2
Loverde <w.andrew.loverde@gmail.com>
2023-10-16 15:05:09 -0500

    heroku deployment error correction attmept

--- server/server.js
+++ server/server.js
@@ -18,12 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
-});
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
