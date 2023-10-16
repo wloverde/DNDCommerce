a8c692fcea294607d4aff5008092c245ac155dba
Loverde <w.andrew.loverde@gmail.com>
2023-10-16 15:30:01 -0500

    herokus failing to render main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App.jsx'
 
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-
-ReactDOM.render(<React.StrictMode>
-  <App />
-</React.StrictMode>, document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
+
+
+// ReactDOM.render(<React.StrictMode>
+//   <App />
+// </React.StrictMode>, document.getElementById('root')
+// )
