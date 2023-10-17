404b7e82b22d4f45fc9cee74b6a1335cfc45e918
fzhao888 <frank.zhao93@gmail.com>
2023-10-17 17:07:10 -0500

    Added back icons for profile, order history, and logout

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,31 @@
-import './Navbar.css';
-import { useState } from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_CATEGORIES } from '../../../utils/queries';
-import { Link } from 'react-router-dom';
-import twistedTrout from '../../assets/images/twisted-trout.svg';
-import search from '../../assets/images/magnifying-glass.svg'; 
-import account from '../../assets/images/account.png';
-import logout from '../../assets/images/logout.png';
-import orderHistory from '../../assets/images/orderHistory.png';
-import SearchForm from '../SearchForm/SearchForm';
-import Auth from '../../../utils/auth'; 
+import "./Navbar.css";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_CATEGORIES } from "../../../utils/queries";
+import { Link } from "react-router-dom";
+import twistedTrout from "../../assets/images/twisted-trout.svg";
+import search from "../../assets/images/magnifying-glass.svg";
+import account from "../../assets/images/account.png";
+import logout from "../../assets/images/logout.png";
+import orderHistory from "../../assets/images/orderHistory.png";
+import SearchForm from "../SearchForm/SearchForm";
+import Auth from "../../../utils/auth";
 
-const  Navbar = ({ setSelectedCategory, isLoggedIn, currentUser }) => {
+const Navbar = ({ setSelectedCategory, isLoggedIn, currentUser }) => {
   const [displaySearch, setDisplaySearch] = useState(false);
 
   const {
     loading: loadingCategories,
     error: errorCategories,
     data: dataCategories,
-  } = useQuery(QUERY_CATEGORIES); 
+  } = useQuery(QUERY_CATEGORIES);
   const categoryClick = (category) => {
     const categoryId = category._id;
     setSelectedCategory(categoryId); // Call the callback function
   };
 
   if (loadingCategories) {
-    return <span className='loading loading-dots loading-lg'></span>;
+    return <span className="loading loading-dots loading-lg"></span>;
   }
 
   function showNavigation() {
@@ -33,30 +33,38 @@ const  Navbar = ({ setSelectedCategory, isLoggedIn, currentUser }) => {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
+            <Link to={"/profile"} className="tooltip" data-tip="Profile">
+              <img className="h-8 w-8 icons" src={account} />
             </Link>
           </li>
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
+          <li>
+            <Link
+              to={"/orderHistory"}
+              className="tooltip"
+              data-tip="Order History"
+            >
+              <img className="h-8 w-8 icons" src={orderHistory} />
+            </Link>
           </li>
+          <li> 
+          {isLoggedIn ? (
+          <div className="tooltip" data-tip="Logout">
+            <img className="h-8 w-8 icons" src={logout} onClick={Auth.logout} />
+          </div>
+        ) : (
+          <></>
+        )}
+          </li> 
         </ul>
       );
     } else {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       );
@@ -64,9 +72,9 @@ const  Navbar = ({ setSelectedCategory, isLoggedIn, currentUser }) => {
   }
 
   return (
-    <nav className='content-flex header'>
+    <nav className="content-flex header">
       {/* wraps the search icon, then when clicked renders the search bar conditionally */}
-      <div className='search-wrapper'>        
+      <div className="search-wrapper">
         <img
           src={search}
           onClick={() =>
@@ -75,39 +83,27 @@ const  Navbar = ({ setSelectedCategory, isLoggedIn, currentUser }) => {
         />
         {displaySearch ? <SearchForm /> : <></>}
         {isLoggedIn ? (
-          <p style={{ paddingLeft: '5px' }}>Welcome, {currentUser.username}</p>
+          <p style={{ paddingLeft: "5px" }}>Welcome, {currentUser.username}</p>
         ) : (
           <></>
         )}
       </div>
       {/* container for the icons on the navbar, they all link to their respective routes in the app jsx component */}
-      <div className='image-wrapper'>
+      <div className="image-wrapper">
         <span>Twisted</span>
-        <Link to={'/'}>
-          <img className='mask mask-circle bg-white ' src={twistedTrout} />
+        <Link to={"/"}>
+          <img className="mask mask-circle bg-white " src={twistedTrout} />
         </Link>
         <span>Trout</span>
       </div>
       {showNavigation()}
-      <div className='menu menu-horizontal icons-wrapper'> 
-        <Link to={'/profile'} className='tooltip' data-tip='Profile'>
-          <img className='h-8 w-8 icons' src={account} />
-        </Link>
-        <Link to={'/orderHistory'} className='tooltip' data-tip='Order History'>
-          <img className='h-8 w-8 icons' src={orderHistory} />
-        </Link>
+      <div className="menu menu-horizontal icons-wrapper">
         {/* conditionally renders the logout button based on the state passed in from the app jsx component */}
-        {isLoggedIn ? (
-          <div className='tooltip' data-tip='Logout'>
-            <img className='h-8 w-8 icons' src={logout} onClick={Auth.logout} />
-          </div>
-        ) : (
-          <></>
-        )}
+        
       </div>
-      <div className='line-break' />
-      <div className='category-list'>
-        <ul className='categories'>
+      <div className="line-break" />
+      <div className="category-list">
+        <ul className="categories">
           {/* render the cagetories with a query */}
           {dataCategories.categories.map((category) => (
             <li key={category._id} onClick={() => categoryClick(category)}>
@@ -116,8 +112,8 @@ const  Navbar = ({ setSelectedCategory, isLoggedIn, currentUser }) => {
           ))}
         </ul>
       </div>
-      <div className='text-center slogan-text'>
-        <p className='text-3xl font-bold'>
+      <div className="text-center slogan-text">
+        <p className="text-3xl font-bold">
           ✨Trout has the sale if you have the scales. We always roll nat
           20&#39;s so delivery is instant using Teleportation!✨
         </p>
