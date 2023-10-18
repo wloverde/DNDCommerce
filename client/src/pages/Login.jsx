624aca142440b2f14083032ad9efd971a94b0ee6
Loverde <w.andrew.loverde@gmail.com>
2023-10-18 15:45:15 -0500

    fixed login and signup backgrounds/input sizing

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
   };
 
   return (
-    <div className='hero min-h-screen bg-base-200'>
+    <div className='hero min-h-screen'>
       <div className='hero-content flex-col lg:flex-row-reverse'>
         <div className='text-center lg:text-left'>
           <h1 className='text-5xl font-bold'>Login now!</h1>
